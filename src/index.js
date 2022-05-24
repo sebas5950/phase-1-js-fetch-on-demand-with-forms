591e2dccbc62440e274a99f10c883c4adeaf7322
Alex Guanipatin <sebas5950@gmail.com>
2022-05-24 19:08:43 -0400

    commit tree 4ae8cb76a0bc3f41fbc482d6408dfb443e66a587

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 const init = () => {
-  
+  document.querySelector('form').addEventListener('submit', (e) => {
+      e.preventDefault()
+      const input = e.target.children[1].value
+      console.log(input)
+    
+
+       fetch(`http://localhost:3000/movies/${input}`)
+      .then(res => res.json())
+      .then(books => inputBookText(books))
+
+    function inputBookText(books) {
+
+        const h4 = document.querySelector('#movieDetails h4')
+      const p = document.querySelector('#movieDetails p')
+      h4.textContent = books.title
+      p.textContent = books.summary
+
+    }
+  })
 }
 
 document.addEventListener('DOMContentLoaded', init);
